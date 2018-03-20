0f2d29982ffcfd834fb1ac25f1530069edb8b9ef
Prabhat Yadav <31845530+prabhu1122@users.noreply.github.com>
2018-03-20 14:03:45 +0530

    added examples

---
+++ Ultrasonic.ino
@@ -0,0 +1,12 @@
+#include <Ultraprabhu.h>
+Ultrasenc ultra(10,11);
+
+float feetDist;
+void setup() {
+  ultra.begin(9600);
+}
+void loop() {
+  //ultra.distFind();
+  feetDist = ultra.distFind_feet();
+  Serial.println(feetDist);
+}
