30817cd2fca7888c5a1560bac836703b20c24639
Prabhat Yadav <31845530+prabhu1122@users.noreply.github.com>
2018-03-20 13:54:21 +0530

    added libraries

---
+++ Ultraprabhu.cpp
@@ -0,0 +1,74 @@
+#include "Ultraprabhu.h"            //include the library for ultasenc
+
+Ultrasenc::Ultrasenc(int x,int y){  //create a constructor for the class Ultrasenc
+  
+  _trig = x;
+  _echo = y; 
+}
+
+//
+
+void Ultrasenc::distFind(){
+
+  digitalWrite(_trig,HIGH);  //sets the trigger pin high or 1;
+  delay(10);                 // put a delay of 10 millisecond
+  digitalWrite(_trig,LOW);   //sets the trigger pin low or 0;
+
+  _duration = pulseIn(_echo,HIGH)/2;   //check the duration of echopin to be high   
+
+  float cm = _duration/29;
+  float inch = _duration/72;
+  float feet = _duration/864;
+  Serial.print("cm: ");
+  Serial.print(cm);
+  Serial.print(", feet: ");
+  Serial.print(feet);
+  Serial.print(", inch: ");
+  Serial.println(inch);
+  
+}
+
+float Ultrasenc::distFind_cm(){
+
+  digitalWrite(_trig,HIGH);
+  delay(10);
+  digitalWrite(_trig,LOW);
+
+  _duration = pulseIn(_echo,HIGH)/2;
+  cm = _duration/29;
+
+  return cm;
+}
+
+
+float Ultrasenc::distFind_inch(){
+
+  digitalWrite(_trig,HIGH);
+  delay(10);
+  digitalWrite(_trig,LOW);
+
+  _duration = pulseIn(_echo,HIGH)/2;
+  inch = _duration/72;
+
+  return inch;
+}
+
+float Ultrasenc::distFind_feet(){
+
+  digitalWrite(_trig,HIGH);
+  delay(10);
+  digitalWrite(_trig,LOW);
+
+  _duration = pulseIn(_echo,HIGH)/2;
+  feet = _duration/(72*12);
+
+  return feet;
+}
+
+void Ultrasenc::begin(int baudRate){
+  Serial.begin(baudRate);
+  pinMode(_trig,OUTPUT);
+  pinMode(_echo,INPUT);
+}
+
+
---
+++ Ultraprabhu.h
@@ -0,0 +1,37 @@
+#ifndef tl
+#define tl
+
+ #if(ARDUINO >= 100)
+   #include "Arduino.h"
+ #else
+   #include "WProgram"
+ #endif
+ 
+//creat a class 
+
+class Ultrasenc{              
+  //code written in this section can be use in main sketch
+  public:
+  //public Properties
+         int x;
+         int y;
+         float cm;
+         float inch;
+         float feet;
+  //public Methods
+         Ultrasenc(int,int);                //constructor
+         void distFind();                   //method for find distance has no return type 
+         float distFind_cm();               //another method to find dist in cm and return type is float
+         float distFind_inch();             //another method to find dist in inch and return type is float 
+         float distFind_feet();             //another method to find dist in feet and return type is float 
+         void begin(int baudRate = 9600);   //set the baudRate for serial moniter default value is 9600
+
+  private:
+  //code written in this section can't be use in main sketch
+  //Private Properties 
+         unsigned long _duration;
+         int _trig;
+         int _echo; 
+  //Private methods 
+};
+#endif 
