416a52abe077b6034a96082a343cc51f77a1c51b
Prabhat Yadav <31845530+prabhu1122@users.noreply.github.com>
2018-04-13 12:55:55 +0530

    Add comments

--- Ultrasonic.ino
+++ Ultrasonic.ino
@@ -1,12 +1,13 @@
 #include <Ultraprabhu.h>
 Ultrasenc ultra(10,11); //ultra(triggerPin,echoPin);
 
-float feetDist;
+float feetDist = 0;     //initialise feetDist
 void setup() {
-  ultra.begin(9600);
+  ultra.begin(9600);    //set baudRate 9600
 }
 void loop() {
-  //ultra.distFind();
+  //call function DistFind_Feet() to find dist in feet;
   feetDist = ultra.distFind_feet();
+  //Print value of distance in feet on serial monitor;
   Serial.println(feetDist);
 }
