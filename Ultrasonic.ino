8ebdabffa78d397336b0445050d63c692fe3ff84
Prabhat Yadav <prabhat199541@gmail.com>
2019-04-21 15:30:53 +0530

    put all method in code

--- Ultrasonic.ino
+++ Ultrasonic.ino
@@ -1,23 +1,33 @@
 #include <Ultraprabhu.h>
 
-Ultrasenc ultra(10,11); //ultra(triggerPin,echoPin);
+Ultrasenc ultra(12,11);             //ultra(triggerPin,echoPin);
 
-float feetDist = 0;     //initialise feetDist
+float feetDist,cmDist,inchDist;     //initialise feetDist
 
 void setup() {
   
-  ultra.begin(9600);    //set baudRate 9600
+  ultra.begin(9600);               //set baudRate 9600
   
 }
 
 void loop() {
+
+  ultra.distFind();
   
   //call function DistFind_Feet() to find dist in feet;
-  
   feetDist = ultra.distFind_feet();
+  
+  cmDist = ultra.distFind_cm();
+  
+  inchDist = ultra.distFind_inch();
   
   //Print value of distance in feet on serial monitor;
   
+  Serial.print("Feet: ");
   Serial.println(feetDist);
-  
+  Serial.print("cm: ");
+  Serial.println(cmDist);
+  Serial.print("Inch: ");
+  Serial.println(inchDist);
+  delay(500);
 }
